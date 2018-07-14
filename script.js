35f03ac6bb59634a50a56187e77b0306c1e91c78
Laura Shamus <lkshamus@gmail.com>
2018-07-14 13:46:33 -0600

    Add appending and delete JS function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var saveBtn = document.querySelector('.save-btn');
 var storedItems = [];
+var ideaTitle = document.querySelector('.idea-title');
+var ideaBody = document.querySelector('.idea-body');
+var unorderedList = document.querySelector('ul');
 
 function Idea(title, idea, quality) {
   this.title = title;
@@ -18,6 +21,7 @@ function saveIdea(e) {
     storedItems.push(idea);
     console.log(storedItems);
     localStorage.setItem('storedItems', JSON.stringify(storedItems));
+    appendList()
   }
 }
 
@@ -26,3 +30,33 @@ function appendElement(obj) {
 }
 
 saveBtn.addEventListener('click', saveIdea);
+
+
+function appendList() {
+    var newIdea = document.createElement('li');
+    if (ideaTitle.value == 0 || ideaBody.value == 0) {
+        newIdea.innerText = null
+    } else {
+        newIdea.innerHTML =`
+        <h3>${ideaTitle.value}</h3>
+        <span> <button class="delete">
+        <img class="delete-btn"src="images/delete.svg"></button></span>
+        <p>${ideaBody.value}</p>
+        <span class="up-arrow"><button class="up">
+        <img class= "down" src="images/upvote.svg"></button></span>
+        <span class="down-arrow">
+        <button class="down"><img src="images/downvote.svg"></button></span>
+        <span class="quality-rate">quality:</span>`
+        var listIdeas = document.querySelector("ul");
+        listIdeas.appendChild(newIdea);
+    }
+}
+unorderedList.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (e.target.className === 'delete-btn') {
+        e.target.parentNode.parentNode.parentNode.remove(document.querySelector('ul'));
+    }
+    console.log('delete me')
+    console.log(e)
+})
+
