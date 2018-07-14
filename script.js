9c617ab2fdb540d6433b8ca7e7211ed44697498d
marcusp619 <marcusp619@gmail.com>
2018-07-14 14:26:49 -0600

    Fix structure of JS

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
+var ideaBody = document.querySelector('.idea-body');
+var ideaTitle = document.querySelector('.idea-title');
 var saveBtn = document.querySelector('.save-btn');
 var storedItems = [];
-var ideaTitle = document.querySelector('.idea-title');
-var ideaBody = document.querySelector('.idea-body');
 var unorderedList = document.querySelector('ul');
-
-function Idea(title, idea, quality) {
-  this.title = title;
-  this.idea = idea;
-  this.quality = quality;
-}
-
-function saveIdea(e) {
-  e.preventDefault();
-  var ideaTitle = document.querySelector('.idea-title').value;
-  var ideaBody = document.querySelector('.idea-body').value;
-
-  var idea = new Idea(ideaTitle, ideaBody);
-  appendElement(idea);
-  if (localStorage.getItem('idea') === null){    
-    storedItems.push(idea);
-    console.log(storedItems);
-    localStorage.setItem('storedItems', JSON.stringify(storedItems));
-    appendList();
-    clearOut();
-  }
-}
-
-function appendElement(obj) {
- console.log(obj);
-}
-
-saveBtn.addEventListener('click', saveIdea);
 
 function appendList() {
     var newIdea = document.createElement('li');
@@ -51,6 +23,34 @@ function appendList() {
         listIdeas.appendChild(newIdea);
     }
 }
+
+function clearOut() {
+  ideaTitle.value = '';
+  ideaBody.value = '';
+}
+
+function Idea(title, idea, quality) {
+  this.title = title;
+  this.idea = idea;
+  this.quality = quality;
+}
+
+function saveIdea(e) {
+  e.preventDefault();
+  var ideaTitle = document.querySelector('.idea-title').value;
+  var ideaBody = document.querySelector('.idea-body').value;
+  var idea = new Idea(ideaTitle, ideaBody);
+  if (localStorage.getItem('idea') === null){    
+    storedItems.push(idea);
+    console.log(storedItems);
+    localStorage.setItem('storedItems', JSON.stringify(storedItems));
+    appendList();
+    clearOut();
+  }
+}
+
+saveBtn.addEventListener('click', saveIdea);
+
 unorderedList.addEventListener('click', function(e) {
     e.preventDefault();
     if (e.target.className === 'delete-btn') {
@@ -59,10 +59,3 @@ unorderedList.addEventListener('click', function(e) {
     console.log('delete me')
     console.log(e)
 })
-
-function clearOut() {
-  ideaTitle.value = '';
-  ideaBody.value = '';
-}
-
-
