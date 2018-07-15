d91b9eca40f794b3d2d59a3ebf416745688a9308
marcusp619 <marcusp619@gmail.com>
2018-07-15 09:59:37 -0600

    hover affect on upvote btn

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var ideaTitle = document.querySelector('.idea-title');
 var saveBtn = document.querySelector('.save-btn');
 var storedItems = [];
 var unorderedList = document.querySelector('ul');
+var upBtn = document.querySelector('.up');
 
 function appendList() {
     var newIdea = document.createElement('li');
@@ -15,8 +16,8 @@ function appendList() {
           <span class="delete-container"><button class="delete">
           <img class="delete-btn"src="images/delete.svg" ></button></span>
           <p contenteditable>${ideaBody.value}</p> 
-          <span class="up-arrow"><button class="up">
-          <img src="images/upvote.svg"></button></span>
+          <span class="up-arrow"><button class="up"><img src="images/upvote.svg">
+          </button></span>
           <span class="down-arrow">
           <button class="down"><img class="down"src="images/downvote.svg"></button></span>
           <span class="quality-rate">quality:</span>
@@ -34,7 +35,7 @@ function clearOut() {
 function Idea(title, idea, quality) {
   this.title = title;
   this.idea = idea;
-  this.quality = quality;
+  this.quality = 'swill';
 }
 
 function saveIdea(e) {
@@ -42,6 +43,7 @@ function saveIdea(e) {
   var ideaTitle = document.querySelector('.idea-title').value;
   var ideaBody = document.querySelector('.idea-body').value;
   var idea = new Idea(ideaTitle, ideaBody);
+  console.log(localStorage.getItem('idea'));
   if (localStorage.getItem('idea') === null){    
     storedItems.push(idea);
     console.log(storedItems);
@@ -57,7 +59,14 @@ unorderedList.addEventListener('click', function(e) {
     e.preventDefault();
     if (e.target.className === 'delete-btn') {
         e.target.parentNode.parentNode.parentNode.remove(document.querySelector('ul'));
-    }
-    console.log('delete me')
-    console.log(e)
+    } 
+});
+
+unorderedList.addEventListener('mouseover', function(e) {
+  e.preventDefault();
+  if (e.target.className === 'up') {
+    e.target.childNodes[0].removeAttribute('src');
+    e.target.childNodes[0].setAttribute('src', 'images/upvote-hover.svg');
+  }
 })
+
